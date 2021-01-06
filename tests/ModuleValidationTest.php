b433cc28f47aaad0e4a2eb967c19f586889beb62
TillBrede <51406030+TillBrede@users.noreply.github.com>
2021-01-06 12:59:45 +0100

    added tests

---
+++ tests/ModuleValidationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class AnwesenheitsSimulationValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateModuleLibrary(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../Module');
+    }
+}
